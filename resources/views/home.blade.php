4be2494c182b93604c8971795a5fe73b06214437
Kimani Kabiria <kabiriakimani@gmail.com>
2019-07-08 09:08:26 +0300

    Refined User registration & Tweaked Homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,26 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link" href="#"><h3>Dashboard</h3></a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#"><h3>Tasks </h3></a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Link 1</a>
+                <a class="dropdown-item" href="#">Link 2</a>
+                <a class="dropdown-item" href="#">Link 3</a>
             </div>
-        </div>
-    </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><h3>Issues</h3></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><h3>My Projects</h3></a>
+        </li>
+    </ul>
+    
 </div>
 @endsection
